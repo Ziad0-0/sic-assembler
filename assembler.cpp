f70a87db346ee64be8925192974272e78795c304
Ziad Khaled <ziadkhaled1419@gmail.com>
2018-10-21 18:12:43 +0200

    finished tokenizing the sic source code

--- assembler.cpp
+++ assembler.cpp
@@ -1,73 +1,75 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 
 using namespace std;
-
+const string no_label = "#";
 int main()
 {
 	ifstream source_file("code.txt");
-	
+
 	vector <string> file_lines;
 
-	//read all the lines in the source code file	
+	//read all the lines in the source code file
 	string line;
 	while(getline(source_file, line))
 		file_lines.push_back(line);
 
-	//Tokenize each line
-	regex instructions_regex("\s*[a-zA-Z]+\s+[a-zA-Z]+\s+[a-fA-F0-9']+");
-	vector<string> first_tokens, second_tokens, third_tokens;
+    	vector<string> labels_tokens, operations_tokens, operands_tokens;
+
+	//Tokenize each line from the source code
+
 	for(string line : file_lines)
 	{
-		cout<<"Instruction: "<<line<<" is ";
+		//cout<<line<<endl;
+       		vector<string> tokens;
+		string current_token = "";
+		bool move_to_next_token = false;
 
-		//if(regex_match(line, instructions_regex))
-		if(true)
-		{
-			cout<<"correct instruction\n";
-			vector<string> tokens(3);
+       		for(int i = 0, l = line.length(); i<l; i++)
+        	{	
 			
-			int j = 0;
-			bool move_to_next_token = false;
 			
-			for(int i = 0, l = line.length(); i<l; i++)
-			{
-				if(line[i] == ' ')
-				{	
-					if(move_to_next_token)
-					{	
-						tokens[j].push_back('\0');
-						j++;
-						move_to_next_token = false;
-					}
-					while(i<l && line[i+1] == ' ')
-						i++;
-				}
-				else
+
+           		if(isspace(line[i]))
+            		{
+				if(move_to_next_token)
 				{
-					tokens[j].push_back(line[i]);
-					move_to_next_token = true;
+					tokens.push_back(current_token);
+                    			current_token = "";
+					move_to_next_token = false;
 				}
+                    		while(i<l && isspace(line[i+1]))
+                    			i++;
 			}
 
-			if(tokens.size() == 2)
-			{
-				first_tokens.push_back("");
-				second_tokens.push_back(tokens[0]);
-				third_tokens.push_back(tokens[1]);
-			}
-			else if(tokens.size() == 3)
-			{
-				first_tokens.push_back(tokens[0]);
-				second_tokens.push_back(tokens[1]);
-				third_tokens.push_back(tokens[2]);
+           		else
+                        {
+				current_token += line[i];
+				move_to_next_token = true;
 			}
 
-		}
-		else
-			cout<<"wrong instruction\n";
+        	}
+
+       		if(tokens.size() == 2) // The case where there aren't any labels for the current instruction
+       		{
+            		string no_label = "#";
+            		labels_tokens.push_back(no_label);
+            		operations_tokens.push_back(tokens[0]);
+            		operands_tokens.push_back(tokens[1]);
+        	}
+        	else if(tokens.size() == 3)
+        	{
+            		labels_tokens.push_back(tokens[0]);
+            		operations_tokens.push_back(tokens[1]);
+            		operands_tokens.push_back(tokens[2]);
+        	}
+
 	}
-		
-	for(int i=0, l = second_tokens.size(); i<l;i++)
-		cout<<first_tokens[i]<<" "<<second_tokens[i]<<" "<<third_tokens[i]<<endl;
+
+
+	for(int i=0, l = operations_tokens.size(); i<l;i++)
+		cout<<labels_tokens[i]<<" "<<operations_tokens[i]<<" "<<operands_tokens[i]<<endl;
+
+	//Construct a symbol table for all the symbolic labels used by the programmer
+	unordered_map<string, string> symbols_table;
 	return 0;
 }
